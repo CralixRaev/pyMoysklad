cf7cd990fc5605717c44e95f64191b1a4b187bd7
Anatoly Raev <cralixraev@gmail.com>
2022-08-09 10:59:17 +0300

    add Barcode class and barcode fields in product and variant

---
+++ pymoysklad/json/objects/barcode.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+from pymoysklad.json.entity import object
+
+
+@dataclass(repr=False)
+class Barcode(object.Object):
+    ean13: str | None = None
+    ean8: str | None = None
+    code128: str | None = None
+    gtin: str | None = None
